caad112b6756ec55ff69ecc2f74c8f52a727abbd
Chetan Kini <chetan.d.kini@gmail.com>
2025-02-09 15:15:40 -0500

    more tests

--- tests/apps/test_receipt_app.py
+++ tests/apps/test_receipt_app.py
@@ -3,13 +3,19 @@ import pytest
 
 from unittest import mock
 from src.apps.receipt_app import ReceiptApp
+from src.providers.receipt_provider import ReceiptProvider
 from src.types.item import ItemWithID
-from src.types.receipt import ReceiptWithIDs
+from src.types.receipt import ReceiptNoIDs, ReceiptWithIDs
 
 
 @pytest.fixture
-def receipt_app() -> ReceiptApp:
-    return ReceiptApp(receipt_provider=mock.Mock())
+def mock_receipt_provider() -> mock.Mock:
+    return mock.Mock(spec=ReceiptProvider)
+
+
+@pytest.fixture
+def receipt_app(mock_receipt_provider: mock.Mock) -> ReceiptApp:
+    return ReceiptApp(receipt_provider=mock_receipt_provider)
 
 
 @pytest.mark.parametrize(
@@ -81,10 +87,45 @@ def receipt_app() -> ReceiptApp:
     ],
 )
 def test_calculate_points(
-    receipt_app: ReceiptApp, receipt: ReceiptWithIDs, expected_points: int
+    receipt_app: ReceiptApp,
+    mock_receipt_provider: mock.Mock,
+    receipt: ReceiptWithIDs,
+    expected_points: int,
 ):
+    # Arrange
     receipt_id = receipt.id
-    receipt_app._receipt_provider.get_by_id.return_value = receipt
+    mock_receipt_provider.get_by_id.return_value = receipt
 
+    # Act
     points = receipt_app.calculate_receipt_points(receipt_id=receipt_id)
+
+    # Assert
     assert points == expected_points
+
+
+def test_add_receipt(receipt_app: ReceiptApp, mock_receipt_provider: mock.Mock):
+    # Arrange
+    receipt = ReceiptNoIDs(
+        retailer="Dunder Mifflin",
+        purchaseDate="2022-01-01",
+        purchaseTime="13:01",
+        total=0,
+        items=[],
+    )
+
+    # Act
+    receipt_app.add_receipt(receipt=receipt)
+
+    # Assert
+    mock_receipt_provider.add_receipt.assert_called_once_with(receipt=receipt)
+
+
+def test_get_receipt(receipt_app: ReceiptApp, mock_receipt_provider: mock.Mock):
+    # Arrange
+    receipt_id = uuid.uuid4()
+
+    # Act
+    receipt_app.get_receipt(receipt_id=receipt_id)
+
+    # Assert
+    mock_receipt_provider.get_by_id.assert_called_once_with(receipt_id=receipt_id)
